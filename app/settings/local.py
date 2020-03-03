3243de2a2949fc11ad37630a8b7dc93e59cf309d
Firstname Lastname <your_email@example.com>
2020-03-03 12:04:17 +0500

    local db changed

--- app/settings/local.py
+++ app/settings/local.py
@@ -15,16 +15,34 @@ ALLOWED_HOSTS = ["*"]
 #     }
 # }
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'jpsocialdb',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
+ 'default': {
+ # 'ENGINE': 'django.db.backends.mysql',
+ 'ENGINE': 'mysql.connector.django',
+ 'NAME': "leanjapan_main", # 接続するDB名
+ 'USER': "leanjapan_miuser", # 接続ユーザ
+ 'PASSWORD': "ZiV7rekJ3uz", # パスワード
+ 'HOST': "mysql8056.xserver.jp", # ホスト名
+ 'PORT': "3306", # ポート番号(3306固定)
+ 'OPTIONS': {
+ # 'charset': 'utf8mb4',
+ 'autocommit': True,
+ }
+ }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'mysql.connector.django',
+#         'NAME': 'jpsocialdb',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 # RunServerPlus
 RUNSERVERPLUS_SERVER_ADDRESS_PORT = '0.0.0.0:8000'
