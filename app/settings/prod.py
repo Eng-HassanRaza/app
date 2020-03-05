82e7565c9df97f2f923fb8dacf9c78d2c3fa7b64
Firstname Lastname <your_email@example.com>
2020-03-05 14:50:42 +0500

    static file changes

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -1,10 +1,11 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-STATIC_URL = '/new_static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # DATABASES = {
 #  'default': {
 #  # 'ENGINE': 'django.db.backends.mysql',
