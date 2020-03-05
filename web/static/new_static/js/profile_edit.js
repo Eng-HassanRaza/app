6d9db19a998d6a4137e7234d3189664cfba6f275
Firstname Lastname <your_email@example.com>
2020-03-05 18:25:17 +0500

    edit_profile new js

--- web/static/new_static/js/profile_edit.js
+++ web/static/new_static/js/profile_edit.js
@@ -7,6 +7,28 @@ $(document).ready(function() {
       reader.readAsDataURL(e.target.files[0]);
   });
 
+  $('#profile_image1').on('change', function (e) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+          $("#preview1").find("img").attr('src', e.target.result);
+      }
+      reader.readAsDataURL(e.target.files[0]);
+  });
+    $('#profile_image2').on('change', function (e) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+          $("#preview2").find("img").attr('src', e.target.result);
+      }
+      reader.readAsDataURL(e.target.files[0]);
+  });
+    $('#profile_image3').on('change', function (e) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+          $("#preview3").find("img").attr('src', e.target.result);
+      }
+      reader.readAsDataURL(e.target.files[0]);
+  });
+
   $('.sns-link').on('click', function (e) {
       var $form = $("#form");
       var url = $(e.currentTarget).data("href");
