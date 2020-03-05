535d7bfca762fac0bd1495fd27b2dd39a6f3bbd9
Firstname Lastname <your_email@example.com>
2020-03-05 18:14:44 +0500

    Edit Profile Fix

--- static/js/profile_edit.js
+++ static/js/profile_edit.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
-  $('#profile_image').on('change', function (e) {
-      var reader = new FileReader();
-      reader.onload = function (e) {
-          $("#preview").find("img").attr('src', e.target.result);
-      }
-      reader.readAsDataURL(e.target.files[0]);
-  });
-
-  $('.sns-link').on('click', function (e) {
-      var $form = $("#form");
-      var url = $(e.currentTarget).data("href");
-
-      $form.attr('action', url);
-      $form.submit();
-  });
-
-  $("input").on('change', function (e) {
-      e.target.blur();
-  });
-});
+// $(document).ready(function() {
+//   $('#profile_image').on('change', function (e) {
+//       var reader = new FileReader();
+//       reader.onload = function (e) {
+//           $("#preview").find("img").attr('src', e.target.result);
+//       }
+//       reader.readAsDataURL(e.target.files[0]);
+//   });
+//
+//   $('.sns-link').on('click', function (e) {
+//       var $form = $("#form");
+//       var url = $(e.currentTarget).data("href");
+//
+//       $form.attr('action', url);
+//       $form.submit();
+//   });
+//
+//   $("input").on('change', function (e) {
+//       e.target.blur();
+//   });
+// });
