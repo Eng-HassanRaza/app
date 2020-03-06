ef08c408abd5bf6685bcb21cc02bbb541e723944
Firstname Lastname <your_email@example.com>
2020-03-06 18:06:20 +0500

    commit changes

--- app/settings/base.py
+++ app/settings/base.py
@@ -139,10 +139,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
 
 # For Logging
 # https://docs.djangoproject.com/en/2.2/topics/logging/
