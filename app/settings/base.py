daf6e6b1c83e3bd2aef65d641efcf93f9979f8e6
Firstname Lastname <your_email@example.com>
2020-03-06 18:10:43 +0500

    root static

--- app/settings/base.py
+++ app/settings/base.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATIC_ROOT = '/static/'
+STATIC_ROOT = '/home/leanjapan/lean_web/new/app/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
