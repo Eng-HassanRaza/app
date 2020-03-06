0622952096794db855a0a82579886d334694ae1c
Firstname Lastname <your_email@example.com>
2020-03-06 20:24:38 +0500

    PRODUCTION DATABASE CHANGED

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -22,13 +22,30 @@ DEBUG = False
 
 
 ####################################### New Database 03/03/2020 #########################################
+# DATABASES = {
+#  'default': {
+#  # 'ENGINE': 'django.db.backends.mysql',
+#  'ENGINE': 'mysql.connector.django',
+#  'NAME': "leanjapan_dev2", # 接続するDB名
+#  'USER': "leanjapan_d2user", # 接続ユーザ
+#  'PASSWORD': "5t66rfCd32", # パスワード
+#  'HOST': "mysql8056.xserver.jp", # ホスト名
+#  'PORT': "3306", # ポート番号(3306固定)
+#  'OPTIONS': {
+#  # 'charset': 'utf8mb4',
+#  'autocommit': True,
+#  }
+#  }
+# }
+
+####################################### New Database 03/06/2020 #########################################
 DATABASES = {
  'default': {
  # 'ENGINE': 'django.db.backends.mysql',
  'ENGINE': 'mysql.connector.django',
- 'NAME': "leanjapan_dev2", # 接続するDB名
- 'USER': "leanjapan_d2user", # 接続ユーザ
- 'PASSWORD': "5t66rfCd32", # パスワード
+ 'NAME': "leanjapan_main", # 接続するDB名
+ 'USER': "leanjapan_miuser", # 接続ユーザ
+ 'PASSWORD': "ZiV7rekJ3uz", # パスワード
  'HOST': "mysql8056.xserver.jp", # ホスト名
  'PORT': "3306", # ポート番号(3306固定)
  'OPTIONS': {
