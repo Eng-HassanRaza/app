76da828c879b2eb0394fdeaba1ac8433e137d64e
Firstname Lastname <your_email@example.com>
2020-03-11 16:57:11 +0500

    main app url change

--- app/urls.py
+++ app/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
     path('accounts/social/login/error/', auth_view.error, name="error"),
     path('accounts/', include('allauth.urls')),
     path('', include("web.urls")),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
