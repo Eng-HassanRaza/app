7b68ecfcc12f63f641b381da9bd67154bd226ddc
Firstname Lastname <your_email@example.com>
2020-03-16 21:24:40 +0500

    production database updated

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -27,9 +27,9 @@ DATABASES = {
  'default': {
  # 'ENGINE': 'django.db.backends.mysql',
  'ENGINE': 'mysql.connector.django',
- 'NAME': "leanjapan_dev2", # 接続するDB名
- 'USER': "leanjapan_d2user", # 接続ユーザ
- 'PASSWORD': "5t66rfCd32", # パスワード
+ 'NAME': "leanjapan_dev3", # 接続するDB名
+ 'USER': "leanjapan_d3user", # 接続ユーザ
+ 'PASSWORD': "xsxx55ttttvfr32", # パスワード
  'HOST': "mysql8056.xserver.jp", # ホスト名
  'PORT': "3306", # ポート番号(3306固定)
  'OPTIONS': {
